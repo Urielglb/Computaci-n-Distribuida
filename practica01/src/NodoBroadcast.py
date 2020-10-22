7feb6ac5f555b65a6d9c7805affad2fbb4e7c3af
Uriel García Luna Bobadilla <uriel_06@ciencias.unam.mx>
2020-10-22 17:20:04 -0500

    Ejercicio Broadcast y documentación vecinos lista

--- practica01/src/NodoBroadcast.py
+++ practica01/src/NodoBroadcast.py
@@ -2,7 +2,9 @@ import simpy
 import Canales.CanalGenerico as CanalGenerico
 from Nodo import Nodo
 
-class NodoVecinos(Nodo):
+
+
+class NodoBroadcast(Nodo):
     """
     Implementa la interfaz Nodo para el algortimo de mandar los vecinos 
     Atributes
@@ -15,40 +17,40 @@ class NodoVecinos(Nodo):
     El objeto Store de la biblioteca simpy para recibir mensajes de los distintos nodos
     canal_salida:simpy.Store
     El objeto Store de la biblioteca simpy para recibir mensajes de los distintos nodos
-    identifiers: set
-    La estructura set que nos servirá para guardar los conjuntos de vecinos  de toda nuestra gráfica
     Methods
     -------
-    manda_vecino
+    broadcast
     El método principal que implementa cada nodo de la gráfica
-    -------
     """
     def __init__(self,id_nodo,vecinos,canal_entrada,canal_salida):
         """
         Inicializa los atributos del nodo
         """
         self.id_nodo = id_nodo
-        self.vecinos = vecinos
+        self.hijos = vecinos #Esto gracias a que las graficas que nos pasan suponemos son árboles
         self.canal_entrada = canal_entrada
         self.canal_salida = canal_salida
         #Atributos para el algoritmo
-        self.identifiers = set() 
-    
-    def manda_vecinos(self,env):
+        self.mensaje = ""
+
+    def broadcast(self,env):
         """
-        Método con el cual los nodos mandrán a cada uno de sus vecinos su lista propia de vecino y a su vez guardará 
-        los nodos de los vecinos que reciba en identifiers
+        Método con el nodo espacial mandará un mensaje a sus hijos y los demás nodos aguardarán a recibir un mensaje y reenviarlo 
+        a sus hijos
         Parameters
         ----------
         env: simpy.Enviroment
         El ambiente de simpy donde se está llevando a cabo la ejecución de todos los nodos
         """
-        conjunto_vecinos = set(self.vecinos)
-        self.canal_salida.envia(conjunto_vecinos,self.vecinos)
+        if(self.get_id()==0):
+            data = ("Un proceso para gobernarlos a todos \n Un proceso para encontrarlos, \n un anillo para atraerlos a todos y atraerlos en las tinieblas")
+            self.mensaje = data
+            self.canal_salida.envia(data,self.hijos)
         while True:
             yield env.timeout(1)
-            mensaje = yield self.canal_entrada.get()
-            self.identifiers = self.identifiers.union(mensaje)
+            data =  yield self.canal_entrada.get()
+            self.mensaje = data
+            self.canal_salida.envia(data,self.hijos)
 
 
     def get_id(self):
@@ -60,16 +62,3 @@ class NodoVecinos(Nodo):
             EL id del nodo
         """
         return self.id_nodo
-
-if __name__ == "__main__":
-    env = simpy.Environment()
-    bc_pipe = CanalGenerico.CanalGenerico(env)
-    grafica = []
-    ady = [[1,3],[0,4],[3],[0,2],[1,5],[4]]
-    for i in range (len(ady)):
-        grafica.append(NodoVecinos(i,ady[i],bc_pipe.crea_canal_de_entrada(),bc_pipe))
-    for i in range (len(ady)):
-        env.process(grafica[i].manda_vecinos(env))
-    env.run()
-    for nodo in grafica:
-        print(("Nodo {} con identifiers {}").format(nodo.get_id(),nodo.identifiers))
